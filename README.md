9f9361b3eb5ff47ae58f1e3d135c2ac300235e5d
Kiss, Tibor <tibor.kiss01@sap.com>
2024-01-30 14:20:35 +0100

    Version: 1.0.4

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # wp-plugin-demo
 Demo WP Plugin
+
+
+## Installation
+
+1. Login to your WordPress admin panel, navigate to Plugins, and click Add New.
+2. Upload the required zip file from builds folder.
+
+## Development
+
+### Clean builds
+
+```bash
+make clean
+```
+
+### Build
+
+```bash
+make build
+```
+
+### Full build (clean & build)
+
+```bash 
+make full-build
+```
--- demo-cloudsteak/demo-cloudsteak.php
+++ demo-cloudsteak/demo-cloudsteak.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Maintenance Mode (CloudSteak)
  * Description: Simple maintenance mode plugin.
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: CloudSteak
  */
 
@@ -21,54 +21,66 @@
 function mm_settings_page() {
     wp_enqueue_media();
 ?>
-<div class="wrap">
-<h1>Maintenance Mode Settings</h1>
-<form method="post" action="options.php">
-    <?php settings_fields('mm-settings-group'); ?>
-    <?php do_settings_sections('mm-settings-group'); ?>
-    <table class="form-table">
-        <tr valign="top">
-        <th scope="row">Background Image</th>
-        <td>
-            <input type="text" id="mm_background_image" name="mm_background_image" value="<?php echo esc_attr(get_option('mm_background_image')); ?>" />
-            <button type="button" class="button" id="mm_upload_image_button">Upload Image</button>
-        </td>
-        </tr>
-         
-        <!-- Additional fields here -->
-    </table>
-    
-    <?php submit_button(); ?>
-</form>
-</div>
-<script>
-jQuery(document).ready(function($){
-    $('#mm_upload_image_button').click(function(e) {
-        e.preventDefault();
-        var image = wp.media({ 
-            title: 'Upload Image',
-            multiple: false
-        }).open()
-        .on('select', function(e){
-            var uploaded_image = image.state().get('selection').first();
-            var image_url = uploaded_image.toJSON().url;
-            $('#mm_background_image').val(image_url);
+    <div class="wrap">
+    <h1>Maintenance Mode Settings</h1>
+    <form method="post" action="options.php">
+        <?php settings_fields('mm-settings-group'); ?>
+        <?php do_settings_sections('mm-settings-group'); ?>
+        <table class="form-table">
+            <tr valign="top">
+            <th scope="row">Background Image</th>
+            <td>
+                <input type="text" id="mm_background_image" name="mm_background_image" value="<?php echo esc_attr(get_option('mm_background_image')); ?>" />
+                <button type="button" class="button" id="mm_upload_image_button">Upload Image</button>
+            </td>
+            </tr>
+             
+            <tr valign="top">
+            <th scope="row">Header Text</th>
+            <td><input type="text" name="mm_header_text" value="<?php echo esc_attr(get_option('mm_header_text')); ?>" /></td>
+            </tr>
+
+            <tr valign="top">
+            <th scope="row">Custom Text</th>
+            <td><textarea name="mm_custom_text"><?php echo esc_textarea(get_option('mm_custom_text')); ?></textarea></td>
+            </tr>
+            <tr valign="top">
+            <th scope="row">Comment</th>
+            <td><label>Select a background image then configure the maintenance mode text.</label></td>
+            </tr>
+        </table>
+        
+        <?php submit_button(); ?>
+    </form>
+    </div>
+    <script>
+    jQuery(document).ready(function($){
+        $('#mm_upload_image_button').click(function(e) {
+            e.preventDefault();
+            var image = wp.media({ 
+                title: 'Upload Image',
+                multiple: false
+            }).open()
+            .on('select', function(e){
+                var uploaded_image = image.state().get('selection').first();
+                var image_url = uploaded_image.toJSON().url;
+                $('#mm_background_image').val(image_url);
+            });
         });
     });
-});
-</script>
+    </script>
  <?php
  }
  
  function mm_maintenance_mode() {
-     if (!current_user_can('edit_themes') || !is_user_logged_in()) {
-         $background_image = get_option('mm_background_image');
-         $header_text = get_option('mm_header_text');
-         $custom_text = get_option('mm_custom_text');
-         
-         wp_die('<div style="background-image:url('.esc_url($background_image).');height:100vh;background-size:cover;"><h1>'.esc_html($header_text).'</h1><p>'.esc_html($custom_text).'</p></div>');
-     }
- }
- 
- add_action('template_redirect', 'mm_maintenance_mode');
- ?>
+    if (!current_user_can('edit_themes') || !is_user_logged_in()) {
+        $background_image = get_option('mm_background_image');
+        $header_text = get_option('mm_header_text');
+        $custom_text = get_option('mm_custom_text');
+
+        wp_die('<div style="background: url('.esc_url($background_image).') no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; height: 100vh; display: flex; justify-content: center; align-items: center; text-align: center;"><h1>'.esc_html($header_text).'</h1><p>'.esc_html($custom_text).'</p></div>');
+    }
+}
+
+add_action('template_redirect', 'mm_maintenance_mode');
+?>
