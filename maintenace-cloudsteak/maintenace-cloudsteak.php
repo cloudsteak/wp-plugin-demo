93f1f8c737b9e2b61246a307e3528e78138a783a
Kiss, Tibor <tibor.kiss01@sap.com>
2024-01-30 14:43:25 +0100

    Version: 1.1.1

--- maintenace-cloudsteak/maintenace-cloudsteak.php
+++ maintenace-cloudsteak/maintenace-cloudsteak.php
@@ -2,8 +2,14 @@
 /**
  * Plugin Name: Maintenance Mode (CloudSteak)
  * Description: Simple maintenance mode plugin.
- * Version: 1.0.4
+ * Version: 1.1.1
  * Author: CloudSteak
+ * Author URI: https://cloudsteak.com
+ * License: MIT
+ * Plugin URI: https://github.com/cloudsteak/wp-plugin-demo
+ * Requires PHP: 7.4
+ * Requires at least: 6.2
+ * Tested up to: 6.4.2
  */
 
  add_action('admin_menu', 'mm_plugin_menu');
@@ -15,11 +21,15 @@
  function register_mm_settings() {
     register_setting('mm-settings-group', 'mm_background_image');
     register_setting('mm-settings-group', 'mm_header_text');
+    register_setting('mm-settings-group', 'mm_header_text_color');
     register_setting('mm-settings-group', 'mm_custom_text');
+    register_setting('mm-settings-group', 'mm_custom_text_color');
 }
  
 function mm_settings_page() {
     wp_enqueue_media();
+    wp_enqueue_style('wp-color-picker');
+    wp_enqueue_script('wp-color-picker');
 ?>
     <div class="wrap">
     <h1>Maintenance Mode Settings</h1>
@@ -41,8 +51,18 @@ function mm_settings_page() {
             </tr>
 
             <tr valign="top">
+            <th scope="row">Header Text Color</th>
+            <td><input type="text" name="mm_header_text_color" value="<?php echo esc_attr(get_option('mm_header_text_color')); ?>" class="color-picker" data-default-color="#000000" /></td>
+            </tr>
+            
+            <tr valign="top">
             <th scope="row">Custom Text</th>
             <td><textarea name="mm_custom_text"><?php echo esc_textarea(get_option('mm_custom_text')); ?></textarea></td>
+            </tr>
+
+            <tr valign="top">
+            <th scope="row">Custom Text Color</th>
+            <td><input type="text" name="mm_custom_text_color" value="<?php echo esc_attr(get_option('mm_custom_text_color')); ?>" class="color-picker" data-default-color="#000000" /></td>
             </tr>
             <tr valign="top">
             <th scope="row">Comment</th>
@@ -53,8 +73,9 @@ function mm_settings_page() {
         <?php submit_button(); ?>
     </form>
     </div>
-    <script>
+    <script type="text/javascript">
     jQuery(document).ready(function($){
+        $('.color-picker').wpColorPicker();
         $('#mm_upload_image_button').click(function(e) {
             e.preventDefault();
             var image = wp.media({ 
@@ -78,7 +99,10 @@ function mm_settings_page() {
         $header_text = get_option('mm_header_text');
         $custom_text = get_option('mm_custom_text');
 
-        wp_die('<div style="background: url('.esc_url($background_image).') no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; height: 100vh; display: flex; justify-content: center; align-items: center; text-align: center;"><h1>'.esc_html($header_text).'</h1><p>'.esc_html($custom_text).'</p></div>');
+        // Enqueue the CSS file
+        wp_enqueue_style('maintenace-cloudsteak', plugins_url('./css/maintenace-cloudsteak.css', __FILE__));
+
+        wp_die('<div class="maintenance-mode" style="background-image: url('.esc_url($background_image).');"><h1 style="color: '.esc_attr($header_text_color).'">'.esc_html($header_text).'</h1><p style="color: '.esc_attr($custom_text_color).'">'.esc_html($custom_text).'</p></div>');
     }
 }
 
